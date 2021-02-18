efad86a5160bc5eca730aaf26f0a09ac79d9c2b4
Kyle Devine <devinenoise@gmail.com>
2021-02-18 12:41:37 -0800

    fullscreen button

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullscreenButton = player.querySelector('.fullscreen-button');
 
 // Build functions
 function togglePlay() {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
 }
 
 function updateButton() {
@@ -22,12 +20,51 @@ function updateButton() {
 }
 
 function skip() {
-  
+  // wrapped with parseFloat because dataset is a string
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function toggleFullScreen() {
+  if (video.fullscreenElement) {
+    video.exitFullscreen();
+  } else if (video.webkitFullscreenElement) {
+    video.webkitFullscreenElement();
+  } else if (video.webkitRequestFullscreen) {
+    video.webkitRequestFullscreen();
+  } else {
+    video.requestFullscreen();
+  }
 }
 
 // Hook up event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+fullscreenButton.addEventListener('click', toggleFullScreen);
 
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progressBar.addEventListener('click', scrub);
+progressBar.addEventListener('mousemove', e => mousedown && scrub(e));
+progressBar.addEventListener('mousedown', () => (mousedown = true));
+progressBar.addEventListener('mouseup', () => (mousedown = false));
